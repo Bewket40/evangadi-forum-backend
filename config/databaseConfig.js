fa2a55b114ec0d23900e47701b01cd9335d333d3
Bewket Wondim <bwondim2014@gmail.com>
2025-07-01 17:10:18 -0400

    renew db

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const dbconfig = {
     host: process.env.DB_HOST , 
-    // port: process.env.DB_PORT || 3306, 
+    port: process.env.DB_PORT , 
     user: process.env.DB_USER ,
     password: process.env.DB_PASSWORD ,
     database: process.env.DB_NAME ,
