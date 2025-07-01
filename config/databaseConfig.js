34dade8b2202bfb1079d04468703a0330da7f7b3
Bewket Wondim <bwondim2014@gmail.com>
2025-07-01 16:36:39 -0400

    port is updated

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -2,11 +2,11 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 const dbconfig = {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '3306',
-    user: process.env.DB_USER || 'evangadi_admin',
-    password: process.env.DB_PASSWORD || '1234567890',
-    database: process.env.DB_NAME || 'evangadi-db',
+    host: process.env.DB_HOST , 
+    // port: process.env.DB_PORT || 3306, 
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME ,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
