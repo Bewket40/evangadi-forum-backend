d65902fdf44718142d6608468d84afece24b78ef
Bewket Wondim <bwondim2014@gmail.com>
2025-07-01 18:22:02 -0400

    something is fix

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,21 +1,21 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+// db.js
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 dotenv.config();
-const dbconfig = {
-    host: process.env.DB_HOST , 
-    user: process.env.DB_USER ,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME ,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
 
-}
-
-// Creating connection pool
-const pool = mysql.createPool(dbconfig);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 export default pool;
+
 
 
 // config/databaseConfig.js
